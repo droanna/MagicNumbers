5ccbeae40a67c4273b6b5b45e3124be7600b7983
Anna <annadrozdz33@gmail.com>
2019-04-01 23:34:39 +0200

    .

--- src/main/java/MagicNumber.java
+++ src/main/java/MagicNumber.java
@@ -1,24 +1,27 @@
 import java.io.*;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MagicNumber {
 
 
     public static List<Integer> magicNumber(File file) throws IOException {
         List<Integer> numbers = new LinkedList<>();
+        byte[] b = new byte[6];
 
         try {
             InputStream inputStream = new FileInputStream(file);
-            int number = inputStream.read();
 
-            while (number != -1) {
+            inputStream.read(b);
+            int i = 0;
+            while (i < b.length) {
+                byte by = b[i];
+                int number = Byte.toUnsignedInt(by);
                 numbers.add(number);
-                number = inputStream.read();
+                i++;
             }
-            return numbers;
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -51,6 +54,27 @@ public class MagicNumber {
             }
         }
         return "Unknown format";
+    }
+
+    private static Map<String, int[]> getExtensions() {
+
+        int[] jpg1 = {0xFF, 0xD8};
+        int[] jpg2 = {0xFF, 0xD9};
+        int[] gif1 = {0x47, 0x49, 0x46, 0x38, 0x39, 0x61};
+        int[] gif2 = {0x47, 0x49, 0x46, 0x38, 0x37, 0x61};
+
+        Map<String, int[]> extensions = new HashMap<>();
+        extensions.put("jpg1", jpg1);
+        extensions.put("jpg2", jpg2);
+        extensions.put("gif1", gif1);
+        extensions.put("gif2", gif2);
+        return extensions;
+    }
+
+    public static void returnType(String fileName) throws IOException {
+        Logger logger = Logger.getLogger(Class.class.getName());
+        List<Integer> magicNumber = magicNumber(new File(fileName));
+        logger.log(Level.INFO, "File type: " + fileType(getExtensions(), magicNumber));
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,19 @@
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main extends MagicNumber {
 
-    private static int[] jpg1 = {0xFF, 0xD8};
-    private static int[] jpg2 = {0xFF, 0xD9};
-    private static int[] gif1 = {0x47, 0x49, 0x46, 0x38, 0x39, 0x61};
-    private static int[] gif2 = {0x47, 0x49, 0x46, 0x38, 0x37, 0x61};
-
-
-    private static Map<String, int[]> extensions = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
 
-        Logger logger = Logger.getLogger(Class.class.getName());
+        returnType("zaba.jpg");
 
-        extensions.put("jpg1", jpg1);
-        extensions.put("jpg2", jpg2);
-        extensions.put("gif1", gif1);
-        extensions.put("gif2", gif2);
+        returnType("SmallFullColourGIF.gif");
 
-        List<Integer> magicNumber = magicNumber(new File("zaba.jpg"));
-        logger.log(Level.INFO, "File type: " + fileType(extensions, magicNumber));
-
-        List<Integer> magicNumber2 = magicNumber(new File("SmallFullColourGIF.gif"));
-        logger.log(Level.INFO, "File type: " + fileType(extensions, magicNumber2));
-
-        List<Integer> magicNumber3 = magicNumber(new File("Przyklad.txt"));
-        logger.log(Level.INFO, "File type: " + fileType(extensions, magicNumber3));
+        returnType("Przyklad.txt");
 
     }
+
+
 }
 
 
